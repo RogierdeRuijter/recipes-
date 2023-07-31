54321bcbbc6908e0a03ed81006965d891ee16757
Rogier <r.de.ruijter@vpro.nl>
2023-07-31 19:08:39 +0200

    add kurkuma rijst

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
-export default {
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
   base: "/recipes-/",
-};
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        doelen: resolve(__dirname, "doelen.html"),
+      },
+    },
+  },
+});
